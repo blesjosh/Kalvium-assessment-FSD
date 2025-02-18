5e9125658de0698a108fdd29edbe152d1bee8ed6
Blessing Joshua <blessingjoshuaoffl@gmail.com>
2025-02-18 10:36:55 +0530

    Done

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json);
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+});
+
+app.get('./signup', (req,res) =>{
+    res.send("Sign up Page")
+
+})
+
+const {username,password,email,dateofbirth} = req.body;
+
+app.post('/signup', (req,res) =>{
+    if (!username){
+        return res.status(400).send("Username cannot be empty");
+    }
+
+    else if(!password){
+        return res.status(400).send("Password cannot be empty")
+    }
+
+    else if(!email){
+        return res.status(400).send("Email cannot be empty")
+    }
+
+    else if(!dateofbirth){
+        return res.status(400).send("Date of birth cannot be empty")
+    }
+
+    else if (!password.length <8 && !password.length >=16){   
+        return res.status(400).send("The password must be greater than 8 and equal to 16 characters")
+}});
+
+    res.status(201).json({
+        message: "User signed up successfully",
+
+    user:{
+        username,
+        email,
+        dateofbirth,
+    }
+});
+
+app.listen(port, () =>{
+    console.lod(`Server is listening to http://localhost:${port}`)
+});
+
